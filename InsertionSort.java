44826f3cf2e3a718acd73ad8b88dc90bce36e36e
ashuuydv <147934359+Ashuuydv@users.noreply.github.com>
2024-04-04 12:35:53 +0530

    insertion sorting method

---
+++ InsertionSort.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class InsertionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array : ");
+        int n = sc.nextInt();
+        System.out.print("enter the digits of the array : ");
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int ele : arr) {
+            System.out.print(ele + " ");
+        }
+        System.out.println();
+
+        //insertion sort
+
+        for(int i = 1; i < n; i++){
+            for(int j = i; j >= 1; j--){
+                if(arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+        }
+
+        for(int ele : arr){
+            System.out.print(ele+" ");
+        }
+    }
+}
